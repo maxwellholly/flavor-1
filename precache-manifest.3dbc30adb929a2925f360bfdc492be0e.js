87c169d7f46381affde3afc5457b7fe852afb311
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2020-06-01 02:44:43 -0700

    Updates

---
+++ precache-manifest.3dbc30adb929a2925f360bfdc492be0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80994ef7e59efe87766d6e096b3a46e9",
+    "url": "/flavor/index.html"
+  },
+  {
+    "revision": "7359a985469c61a11f72",
+    "url": "/flavor/static/css/main.46ab686c.chunk.css"
+  },
+  {
+    "revision": "c402861ed5093abef5f5",
+    "url": "/flavor/static/js/2.e5575223.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flavor/static/js/2.e5575223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7359a985469c61a11f72",
+    "url": "/flavor/static/js/main.beff2151.chunk.js"
+  },
+  {
+    "revision": "0809fe24c9b5a684165c",
+    "url": "/flavor/static/js/runtime-main.163c0dd6.js"
+  },
+  {
+    "revision": "5f680ee14f410f1dda7da09f2cdfaf3c",
+    "url": "/flavor/static/media/flavorgarlic.5f680ee1.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/flavor/precache-manifest.3dbc30adb929a2925f360bfdc492be0e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flavor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
